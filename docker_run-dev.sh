79c1f6b0b2a9a596910ff206523b7fb1e0dba303
Correia Lopes <jlopes@fe.up.pt>
2019-04-01 18:24:31 +0100

    do not seed the db in docker_run-div

--- docker_run-dev.sh
+++ docker_run-dev.sh
@@ -2,19 +2,19 @@
 
 #cd /app
 
-echo "Wait until PostgreSQL is up and ready."
-export PGPASSWORD='pg!lol!2019'
-until pg_isready -h postgres -d postgres -U postgres; do
-  echo "PostgreSQL is unavailable, sleep 10s."
-  sleep 10
-done
-echo "PostgreSQL is ready."
+#echo "Wait until PostgreSQL is up and ready."
+#export PGPASSWORD='pg!lol!2019'
+#until pg_isready -h postgres -d postgres -U postgres; do
+#  echo "PostgreSQL is unavailable, sleep 10s."
+#  sleep 10
+#done
+#echo "PostgreSQL is ready."
 
 if [ ! -f /.inited ]; then
     echo "Since it is the first launch, we will install project dependencies and seed the database."
 
     composer install
-    php artisan db:seed
+#    php artisan db:seed
 
     touch /.inited
 fi
